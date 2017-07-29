7abf67df032758066b37c66cb767533ba6ed69f3
Juliano Felipe <juliano_lpj@hotmail.com>
2017-07-29 15:32:46 -0300

    Herança da classe imagem
    Início da herança entre formatos de imagem (Para ilustrar).

--- PID/image.h
+++ PID/image.h
@@ -63,4 +63,76 @@ class Image{
         ///DEFINE VIRTUAL OPERATOR+ FOR IMAGE SUM IN CHILD CLASSES
 };
 
+//REF. NOTAS DE AULA DE PID :D
+class BMP : Image {
+    public:
+        enum BiCompress_E {
+            BI_RGB  = 0; //Sem compressao
+            BI_RLE8 = 1; //RLE 8 bits
+            BI_RLE4 = 2; //RLE 4 bits
+        }
+
+    private:
+        BITMAPFILEHEADER file_header;
+        BITMAPINFO info;
+        BITMAPINFOHEADER info_header;
+        BYTE* reserved_map;
+
+
+};
+
+//Formato próprio - Mapa de BiTs - MBT
+class MBT : Image {
+    public:
+        //https://stackoverflow.com/questions/8771881/setting-multiple-attributes-for-enum-structure
+        enum Reserved_FH {
+            YUV = 0; //Armazenado em YUV
+            RGB = 1; //Armazenado em RGB
+        }
+
+        ///Campo reservado2 do BMP pode ser usado para
+        ///quando tempos num de bits diferentes entre
+        ///canais, onde cada byte (dos 3 mais significativos)
+        ///indica o tanto de bits por cor. Ex.: 5-6-5
+        ///Reservado2 0101 0110 0101 0000
+        ///             5    6    5        (RGB - 1st, 2nd, 3rd)
+
+    private:
+
+};
+
+typedef struct tagBITMAPFILEHEADER {
+    WORD Type;
+    DWORD Size;
+    WORD Reserved;
+    WORD Reserved2;
+    DWORD OffsetBits;
+} BITMAPFILEHEADER;
+
+typedef struct tagBITMAPINFO {
+    BITMAPINFOHEADER bmiHeader;
+    RGBQUAD bmiColors[1];
+} BITMAPINFO;
+
+typedef struct tagBITMAPINFOHEADER {
+    DWORD biSize;
+    DWORD biWidth;
+    DWORD biHeight;
+    WORD biPlanes;
+    WORD biBitCount;
+    DWORD biCompression;
+    DWORD biSizeImage;
+    DWORD biXPelsPerMeter;
+    DWORD biYPelsPerMeter;
+    DWORD biClrUsed;
+    DWORD biClrImportant;
+} BITMAPINFOHEADER;
+
+typedef struct tagRGBQUAD {
+    BYTE rgbBlue;
+    BYTE rgbGreen;
+    BYTE rgbRed;
+    BYTE rgbReserved;
+} RGBQUAD;
+
 #endif
